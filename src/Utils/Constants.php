2623794ae6054f448880934ed6f8b1723c7e8bb7
Antonio Ferrer <buceocv@gmail.com>
2021-04-11 10:21:57 +0200

    Eliminada constante , no se usará

--- src/Utils/Constants.php
+++ src/Utils/Constants.php
@@ -13,6 +13,5 @@ class Constants
     const postImagePre = 'Image';
     const CLIENT_AVATAR_DIRECTORY = 'avatars';
     const METTERSMULTLIER = 1000;
-    const JSON_CODE_OK = ['action' => 'OK'];
 
 }
